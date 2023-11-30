0c86093dc95948586e39f6ec4964f5f781bdd756
istalia7 <alexis.perrotin02@gmail.com>
2023-11-30 08:53:36 +0100

    home button fix

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ function App() {
           <h1 className="text-xl">
             Perrotin Alexis, Développeur web et web mobile
           </h1>
-          <button className="border-black border-2 rounded-md mt-4 p-1 hover:bg-blue-500">
-            <Link to="/a-propos">En savoir plus</Link>
-          </button>
+          <Link to="/a-propos">
+            <button className="border-black border-2 rounded-md mt-4 p-1 hover:bg-blue-500">
+              En savoir plus
+            </button>
+          </Link>
         </div>
       </div>
     </Layout>
